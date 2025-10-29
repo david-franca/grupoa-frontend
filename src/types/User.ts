c3a7f605d4d84ed89494655c127816924e62a67f
David França <davidemanuel.cf@gmail.com>
2025-10-29 20:19:02 -0300

    feat(types): create all types based on swagger

---
+++ src/types/User.ts
@@ -0,0 +1,15 @@
+export interface User {
+  id: number
+  name: string
+  email: string
+  password: string
+  isActive: boolean
+  created_at: string
+  updated_at: string
+}
+
+export interface UpdateUser {
+  name?: string
+  email?: string
+  password?: string
+}
