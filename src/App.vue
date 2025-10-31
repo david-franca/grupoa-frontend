5f93d076b070558bf2f45795bfecb901161bf448
David França <davidemanuel.cf@gmail.com>
2025-10-31 17:12:53 -0300

    feat: install and configure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary">
-      <v-app-bar-title>CRUD de Alunos</v-app-bar-title>
-    </v-app-bar>
-
-    <v-main>
-      <v-container>
-        <router-view />
-      </v-container>
-    </v-main>
+    <router-view />
   </v-app>
 </template>
-
-<script setup lang="ts">
-// Nenhum script necessário para este layout básico
-</script>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
 import './assets/main.css'
+import 'unfonts.css'
 
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import { registerPlugins } from './plugins'
+import router from './router'
+import authService from './services/authService'
 
 const app = createApp(App)
+
+authService.setupAuthOnAppInit()
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login']
+  const authRequired = !publicPages.includes(to.path)
+
+  const loggedIn = authService.isLoggedIn()
+
+  if (authRequired && !loggedIn) {
+    return next('/login')
+  }
+
+  if (!authRequired && loggedIn) {
+    return next('/students')
+  }
+
+  next()
+})
 
 registerPlugins(app)
 
